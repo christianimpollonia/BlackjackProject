a5ee09f820b5d37853251ce825bf74686dbb4972
Alex <alex@bestalex.com>
2023-10-20 12:08:39 +0100

    Commiting people class

--- People/src/Main.java
+++ People/src/Main.java
@@ -1,5 +1,30 @@
+import java.time.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        LocalDate birthday = LocalDate.of(2000, 10,02);
+        Person christian = new Person ("Christian", "Impollonia", birthday);
+
+        birthday = LocalDate.of(1997, 10,13);
+        Person alexander = new Person ("Alexander", "Burns", birthday);
+
+        birthday = LocalDate.of(1642, 1,2);
+        Person laszlo = new Person ("Laszlo", "Cravensworth", birthday);
+
+        laszlo.getInfo();
+        //Test printing Christian's info
+//        System.out.println(christian.getAge());
+//        System.out.println(christian.getBirthDay());
+//        System.out.println(christian.getFirstName());
+//        System.out.println(christian.getLastName());
+//        System.out.println(christian.getInfo());
+//        System.out.println(Person.returnPopulation());
+//
+//        christian.changeFirstName("Geralt");
+//
+//        System.out.println(christian.getFirstName());
+
+
+
     }
 }
