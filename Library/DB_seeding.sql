18e75f3ec02cdc14dccde78e8d3a14d27bc66e5c
Bryan <bryanneermul@gmail.com>
2023-10-20 14:46:57 +0100

    seeding added

--- Library/DB_seeding.sql
+++ Library/DB_seeding.sql
@@ -1,22 +1,41 @@
-INSERT INTO library.LibraryMember (First_name, Surname, Email, Address, Start_date, Expiry_date, Member_type, Num_books) 
-VALUES ("Stevie", "Wonder", "swonder@jazz.com", "50 Rhythm Road, Hardaway", "1995-03-16", "2000-03-16", "Gold", 0),
-('Jennifer', 'Vega', 'michaelrivera@example.com', '61389 Steele Mews Suite 204 Lake Tiffany, VA 23315', '2022-08-29', '2024-09-08', 'Silver', 5),
-('Katie', 'Coffey', 'browngail@example.com', '92127 Hendricks Village Silvaview, MO 16036', '2023-02-12', '2024-02-11', 'Silver', 1),
-('Crystal', 'Le', 'bjohnson@example.org', '74299 Paul Ridges Suite 115 Lake Stephenport, ME 74695', '2020-03-31', '2025-09-16', 'Gold', 9), 
-('Michelle', 'Williams', 'hluna@example.net', '5375 Brown Crescent South Teresa, TX 83824', '2020-08-18', '2027-07-12', 'Gold', 10), 
-('Wendy', 'Thomas', 'bushbrian@example.org', '2730 Holly Manor Robertoshire, MP 29372', '2020-03-06', '2026-11-05', 'Gold', 5),
-('Robert', 'White', 'ydavis@example.org', '36774 Parker Divide Apt. 527 Port Stephen, NE 85251', '2021-02-05', '2028-02-01', 'Gold', 4),
-('Edward', 'Dougherty', 'fernando95@example.com', '54526 Brandy Villages Huntburgh, NC 58619', '2022-07-25', '2025-12-28', 'Gold', 8),
-('Holly', 'Allen', 'bettymyers@example.net', '01229 Sarah Rapid Suite 339 Jeffreyhaven, NJ 05872', '2023-05-12', '2026-12-20', 'Bronze', 3),
-('William', 'Aguilar', 'nicoletucker@example.net', '96271 Reid Estate Port Lisa, MP 34746', '2019-03-24', '2024-08-15', 'Silver', 4), 
-('David', 'Smith', 'reyesscott@example.com', '10068 Samantha Rest Suite 658 New Brandon, NC 18799', '2020-10-10', '2028-06-18', 'Gold', 5);
+INSERT INTO library.LibraryMember (First_name, Surname, Email, Address, Start_date, Expiry_date, Member_type, Num_books)
+VALUES
+    ('John', 'Doe', 'johndoe@email.com', '123 Main St', '2022-01-01', '2023-01-01', 'Regular', 3),
+    ('Alice', 'Smith', 'alice@email.com', '456 Elm St', '2022-02-15', '2023-02-15', 'Premium', 5),
+    ('Bob', 'Johnson', 'bob@email.com', '789 Oak St', '2022-03-20', '2023-03-20', 'Regular', 2);
 
-SELECT * FROM library.LibraryMember;
+INSERT INTO library.Author (First_name, Surname)
+VALUES
+    ('Jane', 'Doe'),
+    ('Michael', 'Smith'),
+    ('Emily', 'Johnson');
 
-INSERT INTO library.Author (First_name, Surname) 
-VALUES ("William", "Shakespeare"),
-("Stephen", "King"),
-("Joanne", "K. Rowling"),
-("Agatha", "Christie"),
-("Barbara", "Cartland"), 
-("Danielle", "Steel");
+INSERT INTO library.Publisher (Publisher_name)
+VALUES
+    ('Publisher A'),
+    ('Publisher B'),
+    ('Publisher C');
+
+INSERT INTO library.Book (ISBN, Title, Available, Genre, Cover_type, Book_language, Edition)
+VALUES
+    ('1234567890', 'Book 1', 1, 'Fiction', 'Hardcover', 'English', 'First Edition'),
+    ('2345678901', 'Book 2', 1, 'Science', 'Paperback', 'English', 'Second Edition'),
+    ('3456789012', 'Book 3', 1, 'Mystery', 'Hardcover', 'English', 'Third Edition');
+
+INSERT INTO library.PublishedBy (Publisher_id, Book_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+INSERT INTO library.WrittenBy (Author_id, Book_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+INSERT INTO library.BorrowedBy (Book_id, Member_id, Borrowing_date, Return_date)
+VALUES
+    (1, 1, '2023-01-05', '2023-01-15'),
+    (2, 2, '2023-02-10', '2023-02-20'),
+    (3, 3, '2023-03-15', '2023-03-25');
