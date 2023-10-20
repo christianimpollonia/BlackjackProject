963d1461cb27f9d42212e4fd63cde015c2857e7a
Diego <diego@diego.com>
2023-10-20 11:14:13 +0100

    Complete blackjack

--- Blackjack/src/Main.java
+++ Blackjack/src/Main.java
@@ -1,10 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Insert second number");
+        int num2 = scanner.nextInt();
+        int result = play(num1, num2);
+        System.out.println(result);
 
     }
-}
+
+    public static int play(int num1, int num2) {
+        if (num1 > 21 && num2 > 21) {
+            return 0;
+        }
+        else if (num1 > 21) {
+            return num2;
+        }
+        else if (num2 > 21) {
+            return num1;
+        }
+        else return Math.max(num1, num2);
+        }
+    }
