a247d6dfe186c40c6e0cf0e2c77fd677a35fcd75
Alex <alex@bestalex.com>
2023-10-20 11:16:24 +0100

    Final blackjack commit

--- Blackjack/src/Main.java
+++ Blackjack/src/Main.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Insert first number");
         int num1 = scanner.nextInt();
+
         System.out.println("Insert second number");
         int num2 = scanner.nextInt();
+
         int result = play(num1, num2);
         System.out.println(result);
 
